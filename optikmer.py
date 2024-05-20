d6ecc2fb7d4295fce374a28983cf81d60a2b30c4
Amick <amickl29@yahoo.com>
2024-05-20 16:20:45 -0700

    feat: added unique kmer counting

--- optikmer.py
+++ optikmer.py
@@ -1,9 +1,11 @@
 from collections import Counter
 from Bio import SeqIO
+from scipy.signal import argrelextrema
 import random
 import matplotlib.pyplot as plt
 import math
 import argparse
+import numpy as np
 
 # Initialize possible k-mer lengths & empty dictionary for k-mer counts
 kmer_lengths = [11, 21, 31, 41, 51, 61, 71, 81, 91]
@@ -13,21 +15,41 @@ kmer_counts = Counter()
 
 # plot the distribution of kmer counts for specific kmer length
 def plotKmerCounts(k):
-  plt.hist(kmer_counts.values(), bins=range(1, 200))
+  counts, bins, _ = plt.hist(kmer_counts.values(), bins=range(1, 200))
   plt.xlabel('K-mer abundance')
   plt.ylabel('Number of k-mers')
   plt.ylim(0, 200000)
   plt.title(f'k={k}')
   plt.savefig(f'histograms/k{k}.png')
+
+  # Find indices of bins within the specified range
+  bin_indices = (bins[:-1] >= 1) & (bins[:-1] <= 25)
+  
+  # Find indices of local minima within the specified range
+  local_minima = argrelextrema(counts[bin_indices], np.less)
+  
+  # Print local minima
+  start_point = 1
+  for local_min in local_minima[0]:
+    if local_min > start_point:
+      start_point = local_min
+  print(f'k={k}, start_point={start_point}')
+
+  unique_kmers = 0
+  for i in range(start_point, 198):
+    unique_kmers += counts[i]
+    
   plt.close()
+  print (f'k={k}, unique_kmers={unique_kmers}')\
+
 
 def getSampleReads(fastq_file):
-  reads = []
+  reads = [] # while instead of loadin all into memory to save time 
   record_count = 0
   for record in SeqIO.parse(fastq_file, "fastq"):
     reads.append(str(record.seq))
     record_count += 1
-  return random.sample(reads, math.ceil(record_count * 0.75)) # sample 10% of reads
+  return random.sample(reads, math.ceil(record_count * 0.15)) # sample 10% of reads take in as command line arg
   # return random.sample(reads, 10000) # sample 10000
 
 def recordKmers(reads, kmer_len): # also used by kmergenie
