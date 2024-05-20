8924ffb6f200e1c63f0d100347e7d2d124cc0b73
Amick <amickl29@yahoo.com>
2024-05-20 12:44:22 -0700

    feat: updated k11-41 histograms

--- optikmer.py
+++ optikmer.py
@@ -2,11 +2,13 @@ from collections import Counter
 from Bio import SeqIO
 import random
 import matplotlib.pyplot as plt
+import math
 import argparse
 
 # Initialize possible k-mer lengths & empty dictionary for k-mer counts
 kmer_lengths = [11, 21, 31, 41, 51, 61, 71, 81, 91]
 test_kmer_lengths = [18]
+test_kmer_lengths_2 = [19]
 kmer_counts = Counter()
 
 # plot the distribution of kmer counts for specific kmer length
@@ -21,9 +23,11 @@ def plotKmerCounts(k):
 
 def getSampleReads(fastq_file):
   reads = []
+  record_count = 0
   for record in SeqIO.parse(fastq_file, "fastq"):
     reads.append(str(record.seq))
-  return reads
+    record_count += 1
+  return random.sample(reads, math.ceil(record_count * 0.75)) # sample 10% of reads
   # return random.sample(reads, 10000) # sample 10000
 
 def recordKmers(reads, kmer_len): # also used by kmergenie
@@ -45,7 +49,7 @@ def main():
   # Extract reads from each fastq file with getReads function (defined above)
   # and record it in kmer_counts dictionary
   file_reads = getSampleReads(rfiles)
-  for k in test_kmer_lengths:
+  for k in kmer_lengths:
     recordKmers(file_reads, k)
     kmer_counts.clear()
   
